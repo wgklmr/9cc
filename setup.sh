abc37247413dbce0938f718d6d04eadfb352008c
Gakuto Watanabe <gakutow26@gmail.com>
2022-05-21 21:28:12 +0900

    setup.shを修正

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
-docker build -t ubuntu_for_9cc .
+if [[ "$(docker images -q ubuntu_for_9cc 2> /dev/null)" == "" ]]; then
+  docker build -t ubuntu_for_9cc .
+fi
+
 
 docker run -it --mount type=bind,source="$(pwd)",target=/app ubuntu_for_9cc
