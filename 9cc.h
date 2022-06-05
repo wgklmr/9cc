fc7dc11b620cfe0a23b2ee47fa363768a01bc7be
Gakuto Watanabe <gakutow26@gmail.com>
2022-06-05 11:02:28 +0900

    不要コード削除

--- 9cc.h
+++ 9cc.h
@@ -5,25 +5,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-// トークンの種類
 typedef enum {
-  TK_RESERVED, // 記号
-  TK_NUM,      // 整数トークン
-  TK_EOF,      // 入力の終わりを表すトークン
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
 } TokenKind;
 
 typedef struct Token Token;
-
-// トークン型
 struct Token {
-  TokenKind kind; // トークンの型
-  Token *next;    // 次の入力トークン(のポインタ)
-  int val;        // kindがTK_NUMの場合、その数値
-  char *str;      // トークン文字列
+  TokenKind kind;
+  Token *next;
+  int val;
+  char *str;
   int len;
 };
 
-// 抽象構文木のノードの種類
 typedef enum {
   ND_ADD, // +
   ND_SUB, // -
@@ -37,21 +33,21 @@ typedef enum {
 } NodeKind;
 
 typedef struct Node Node;
-
-// 抽象構文木のノードの型
 struct Node {
-  NodeKind kind; // ノードの型
-  Node *lhs;     // 左辺
-  Node *rhs;     // 右辺
-  int val;       // kindがND_NUMの場合のみ使う
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
 };
 
-void error(char *fmt, ...);
-bool consume(char *op);
-void expect(char *op);
 int expect_number();
 bool at_eof();
 bool startswith(char *p, char *q);
+bool consume(char *op);
+void expect(char *op);
+void error(char *fmt, ...);
+void gen(Node *node);
+
 Token *tokenize(char *p);
 Node *new_node(NodeKind kind);
 Node *new_binary(NodeKind kind, Node *lhs, Node *rhs);
@@ -63,7 +59,6 @@ Node *add();
 Node *mul();
 Node *unary();
 Node *primary();
-void gen(Node *node);
 
 extern Token *token;
 extern char *user_input;
